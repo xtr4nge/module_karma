4e6fe76d2bbec255cdd6f127fa528dfc55a2c592
xtr4nge <xtr4nge@gmail.com>
2014-03-24 09:13:33 +0000

    init

---
+++ _info_.php
@@ -0,0 +1,23 @@
+<?
+$mod_name="karma";
+$mod_version="1.0";
+$mod_path="/usr/share/FruityWifi/www/modules/karma";
+$mod_logs="/usr/share/FruityWifi/logs/karma.log"; 
+$mod_logs_history="$mod_path/includes/logs/";
+$mod_panel="show";
+$mod_isup="/usr/sbin/karma-hostapd_cli -p /var/run/hostapd-phy0 karma_get_state | tail -1 | grep ENABLED";
+$mod_alias="Karma";
+# EXEC
+$bin_danger = "/usr/share/FruityWifi/bin/danger";
+$bin_sudo = "/usr/bin/sudo";
+$bin_sh = "/bin/sh";
+$bin_echo = "/bin/echo";
+$bin_grep = "/usr/bin/ngrep";
+$bin_killall = "/usr/bin/killall";
+$bin_cp = "/bin/cp";
+$bin_chmod = "/bin/chmod";
+$bin_sed = "/bin/sed";
+$bin_rm = "/bin/rm";
+$bin_route = "/sbin/route";
+$bin_perl = "/usr/bin/perl";
+?>
---
+++ includes/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "installing Hostapd/Karma Dependencies..."
+# DEP HOSTAPD-KARMA
+apt-get -y install gcc-4.7
+apt-get -y install g++-4.7
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+
+apt-get -y install hostapd
+apt-get -y install libnl1 libnl-dev libssl-dev
+
+echo "installing Hostapd/Karma..."
+# INSTALL HOSTAPD-KARMA
+wget http://www.digininja.org/files/hostapd-1.0-karma.tar.bz2 -O hostapd-1.0-karma.tar.bz2
+
+bunzip2 hostapd-1.0-karma.tar.bz2
+tar xvf hostapd-1.0-karma.tar
+cd hostapd-1.0-karma/hostapd
+make
+
+cp hostapd ../
+cp hostapd_cli ../
+
+echo "..DONE.."
+exit
