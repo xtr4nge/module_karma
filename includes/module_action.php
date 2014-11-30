84ecf44745109c05cc32c6c2458d2f4f03a6fadd
xtr4nge <xtr4nge@gmail.com>
2014-11-30 22:52:09 +0000

    minor update (pre-release v2.1)

--- includes/module_action.php
+++ includes/module_action.php
@@ -1,19 +1,19 @@
 <? 
 /*
-	Copyright (C) 2013-2014  xtr4nge [_AT_] gmail.com
+    Copyright (C) 2013-2014  xtr4nge [_AT_] gmail.com
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 ?>
 <?
@@ -48,11 +48,13 @@ if($service != "") {
         }
 
         $exec = "./hostapd_cli -p /var/run/hostapd-phy0 karma_enable";
+        //$exec = "./hostapd_cli karma_enable";
         exec("/usr/share/FruityWifi/bin/danger \"" . $exec . "\"" );
         
     } else if($action == "stop") {
         // STOP MODULE
         $exec = "./hostapd_cli -p /var/run/hostapd-phy0 karma_disable";
+        //$exec = "./hostapd_cli karma_disable";
         exec("/usr/share/FruityWifi/bin/danger \"" . $exec . "\"" );
         
         // COPY LOG
