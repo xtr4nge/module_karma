ca2cdf2b39d6d6375dccb9023b9486aff5af2316
xtr4nge <xtr4nge@gmail.com>
2015-07-15 19:43:10 +0100

    minor fix (Debian Jessie)

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="karma";
-$mod_version="1.4";
+$mod_version="1.5";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
